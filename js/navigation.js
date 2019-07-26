8e23708b9fcc3869009b36ada6c860aa1a728231
jgersain <jhersein@gmail.com>
2019-07-26 05:44:53 -0500

    refactorización del código

--- js/navigation.js
+++ js/navigation.js
@@ -1,46 +1,62 @@
 $(function() {
 	const extension = ".html"
-	let content = $('#content');
-	let prev_url = ""
-	let original = window.location;
+	const content = $('#content');
+	const htmlError = `<div class="container alert alert-danger m-0" role="alert">La página no existe</div>`
+	let prev_url = '';
 
 	// cambiamos el atributo href por el del contenido del data-page
 	let items = $('nav a');
 	items.each(function() {
 		$(this).attr('href', $(this).data('page'));
 	});
-	
-	// implementación usando solo javascript
-	// [...items].forEach(element => {
-	// 	element.setAttribute('href', element.dataset.page);
-	// });
 
 	// agregamos la funcionalidad a cada item del menú
 	items.on('click', function(event) {
-		let hash = $(this).attr('href');
 		event.preventDefault();
-		validateURL(hash);
+		let hash = $(this).attr('href');
+		// cuando la peticion es satisfactoria
+		validateURL(hash).done(() => window.location.href = hash)
+		// cuando hay un error
+		.fail(() => window.location.href = '#error')
+		// cuando la petición se completo
+		.always(data =>	content.html(data));
 	});
 
+	validateURL();
+	setInterval(function(){
+		validateURL().fail(function(){
+			window.location.href = '#error';
+		}).always(function(data){
+			content.html(data);
+		});
+	}, 250);
+	
 	function validateURL(hash) {
-		// manejando callbacks
+		// manejando callbacks con el objeto referido
 		const deferred = $.Deferred()
 		
-		cargarPagina(hash).done(function(data) {
-			var html = $(data);
-			content.html(html);
-		});
+		// si no hay hash, por ejemplo cuando se navega con lo botones 
+		// del navegador o cuando se introduce manualmente
+		if (!hash)
+			hash = window.location.hash;			
 
+		// cargamos el nuevo contenido solo cuándo es necesario
+		if (hash !== prev_url) {
+			prev_url = hash;
+			cargarPagina(hash)
+			.done(data => deferred.resolve(data))
+			.fail(() => deferred.reject(htmlError));
+		}
 		return deferred.promise();
 	}
 
-	function cargarPagina(hash) {
-		pageUrl = hash.replace('#', '');
+	// relaiza la peticion, retorna una respuesta html
+	function cargarPagina(hash) {		
+		let pageUrl = hash.replace('#', '') + extension;
 		return $.ajax({
-			url: pageUrl + extension,
+			url: pageUrl,
 			async: true,
 			dataType: 'html'
 		});
 	}
-
 });
